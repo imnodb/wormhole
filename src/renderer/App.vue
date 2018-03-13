393a24ce57c08a16ed794d5b08d6d0394c5dd01e
bingur <imnodb@gmail.com>
2018-03-13 19:49:28 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <div @click="startChrome">打开浏览器</div>
+  </div>
+</template>
+
+<script>
+// import path from 'path';
+// import { remote } from 'electron';
+// import startChrome from 'start-chrome';
+
+export default {
+  name: 'wormhole',
+  methods: {
+    startChrome() {
+      global.alert(this.$electron.remote.app.getPath('userData'));
+      // startChrome('');
+    },
+  },
+};
+</script>
+
+<style>
+/* CSS */
+</style>
